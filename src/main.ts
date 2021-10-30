625656dd4e3e3018e44da8dcde229b04f3f9989c
d.pogudin <d.pogudin@sysdyn.ru>
2021-10-30 17:37:06 +0300

    test

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const port = +process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(port, ()=>console.log(`Started on port ${process.env.PORT}`));
+  await app.listen(port, ()=>console.log(`Started on port ${process.env.PORT}!`));
 }
 bootstrap();
