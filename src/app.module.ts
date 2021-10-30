46a461096f2dc6097e1a0f132c1f714cf0ab2881
d.pogudin <d.pogudin@sysdyn.ru>
2021-10-30 20:18:33 +0300

    set ssl for prod

--- src/app.module.ts
+++ src/app.module.ts
@@ -39,13 +39,16 @@ import { ConfigModule } from '@nestjs/config';
                 'database': process.env.POSTGRES_DB,
                 'entities': ['dist/**/*.entity{.ts,.js}'],
                 'synchronize': true,
+                ssl: process.env.NODE_ENV === 'production',
                 extra: {
-                    ssl: true,
+                    ssl: {
+                        rejectUnauthorized: false,
+                    }
                 }
             }
         ),
         ServeStaticModule.forRoot({rootPath: path.resolve(__dirname, 'static')}),
-        ],
+    ],
     controllers: [AppController],
     providers: [AppService
         , {
