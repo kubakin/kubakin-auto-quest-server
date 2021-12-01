7b593bb338ddb7e8109b506822ed36cd5fa9524b
d.pogudin <d.pogudin@sysdyn.ru>
2021-12-01 23:48:06 +0300

    change db

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log(process.env);
   const port = +process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   app.enableCors();
