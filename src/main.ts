5df803d10c5c6dde698896600ddcc61e1e234c22
d.pogudin <d.pogudin@sysdyn.ru>
2022-02-22 00:59:34 +0300

    ura blyat

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   console.log(process.env);
-  const port = +process.env.PORT || 3000;
-  const host = '0.0.0.0';
+  // const port = +process.env.PORT || 3000;
+  const port = 8080 || 3000;
+  // const host = '0.0.0.0';
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(port, host, ()=>console.log(`Started on port ${process.env.PORT}!`));
+  await app.listen(port, ()=>console.log(`Started on port ${process.env.PORT}!`));
 }
 bootstrap();
